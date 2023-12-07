f2b82f14c4acc96ee303b7ed98eba014d9ebd9b9
Zahraa Haider Adnan <zahraaha2019@gmail.com>
2023-12-07 23:43:41 +0100

    changes in myprofile

--- src/Pages/Myprofile.jsx
+++ src/Pages/Myprofile.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import './MyprofileStyle.css';
+import axios from 'axios';
 
 const AuthPage = () => {
     const [isLoginView, setIsLoginView] = useState(true);
+
+    const toggleView = () => {
+        setIsLoginView(!isLoginView);
+    };
 
     return (
         <div className="auth-container">
@@ -17,81 +22,117 @@ const AuthPage = () => {
 };
 
 const LoginForm = () => {
-    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [requests, setRequests] = useState([]);
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        try {
-            const response = await fetch('http://localhost:8080/api/login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password })
-            });
-            const data = await response.json();
-            if (response.ok) {
-                // Handle login success (e.g., redirecting the user, storing auth token)
-            } else {
-                // Handle errors (e.g., displaying a message to the user)
-                console.error(data.message);
+        if (username === "admin" && password === "admin") {
+            setIsAdmin(true);
+            try {
+                const response = await axios.get('http://localhost:8080/api/requests/getall');
+                setRequests(response.data);
+            } catch (error) {
+                console.error('Error fetching requests:', error);
             }
-        } catch (error) {
-            console.error('Login error:', error);
+        } else {
+            alert("Incorrect admin credentials");
         }
     };
 
     return (
-        <form className="auth-form" onSubmit={handleLogin}>
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button type="submit">Login</button>
-        </form>
+        <div>
+            <form className="auth-form" onSubmit={handleLogin}>
+                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Login</button>
+            </form>
+
+            {isAdmin && (
+                <div>
+                    <h2>Received Requests</h2>
+                    {requests.map((request, index) => (
+                        <div key={request._id || index}>
+                            <p><strong>Name:</strong> {request.name}</p>
+                            <p><strong>Email:</strong> {request.email}</p>
+                            <p><strong>Message:</strong> {request.message}</p>
+                            <hr />
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
     );
 };
 
 const RegisterForm = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [dob, setDob] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [repeatPassword, setRepeatPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [isSuccess, setIsSuccess] = useState(false);
 
-    const handleRegister = async (e) => {
-        e.preventDefault();
+    async function save(event) {
+        event.preventDefault();
+        setMessage("");
+        setIsSuccess(false);
+
         if (password !== repeatPassword) {
-            alert("Passwords do not match!");
+            setMessage("Passwords do not match!");
+            setIsSuccess(false);
             return;
         }
+
         try {
-            const response = await fetch('http://localhost:8080/api/register', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ firstName, lastName, dob, email, password })
+            const response = await axios.post("http://localhost:8080/api/v1/user/save", {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
             });
-            const data = await response.json();
-            if (response.ok) {
-                // Handle registration success (e.g., redirecting to login page, showing success message)
+            setMessage("User Registration Successful");
+            setIsSuccess(true);
+            // Clear the form fields
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setPassword("");
+            setRepeatPassword("");
+        } catch (err) {
+            const backendError = err.response && err.response.data ? err.response.data : "UnknownError";
+            if (backendError === "EmailExists") {
+                setMessage("Email already exists.");
             } else {
-                // Handle errors (e.g., displaying a message to the user)
-                console.error(data.message);
+                setMessage("User Registration Failed");
             }
-        } catch (error) {
-            console.error('Registration error:', error);
+            setIsSuccess(false);
         }
-    };
+    }
 
     return (
-        <form className="auth-form" onSubmit={handleRegister}>
+        <form className="auth-form" onSubmit={save}>
             <input type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
             <input type="text" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-            <input type="date" placeholder="Date of Birth" value={dob} onChange={(e) => setDob(e.target.value)} />
             <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
             <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
             <input type="password" placeholder="Repeat Password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
+
+            {message && (
+                <div className={isSuccess ? "auth-message success" : "auth-message error"}>
+                    {message}
+                </div>
+            )}
+
             <button type="submit">Register</button>
         </form>
     );
 };
+
+
+
 
 export default AuthPage;
